9c5fcaf72e437f49094c55961f7e349fe039b998
Santi Lertsumran <santi.lertsumran@gmail.com>
2022-03-19 03:15:37 +0900

    feat: add reactive mongo transaction manager

--- shipment-service/src/main/java/com/mathsermone/store/demo/shipmentservice/ShipmentServiceApplication.java
+++ shipment-service/src/main/java/com/mathsermone/store/demo/shipmentservice/ShipmentServiceApplication.java
@@ -2,6 +2,13 @@ package com.mathsermone.store.demo.shipmentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory;
+import org.springframework.data.mongodb.ReactiveMongoTransactionManager;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @SpringBootApplication
 public class ShipmentServiceApplication {
@@ -10,4 +17,16 @@ public class ShipmentServiceApplication {
 		SpringApplication.run(ShipmentServiceApplication.class, args);
 	}
 
+	@Bean
+	ReactiveMongoTransactionManager transactionManager(ReactiveMongoDatabaseFactory dbFactory) {
+		return new ReactiveMongoTransactionManager(dbFactory);
+	}
+
+	@ControllerAdvice
+	public class ExceptionController {
+		@ExceptionHandler(value = RuntimeException.class)
+		public ResponseEntity<Object> exception(RuntimeException exception) {
+			return new ResponseEntity<>(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 }
