674150f415eec38232a20b79babdaf471ab7b5db
Santi Lertsumran <santi.lertsumran@gmail.com>
2022-03-19 16:51:55 +0900

    feat(frontend): use eventsource for get orders history

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { OrderComponent } from './order/order.component';
 import { LayoutModule } from './layout/layout.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    OrderComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
--- frontend/src/app/services/order.service.ts
+++ frontend/src/app/services/order.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Order } from '../models/order';
@@ -8,13 +8,35 @@ import { Order } from '../models/order';
   providedIn: 'root'
 })
 export class OrderService {
+  orders: Order[] = [];
 
   private orderUrl = 'orders';
 
-  constructor(private http: HttpClient) { }
+  constructor(private _zone: NgZone, private http: HttpClient) { }
 
   getOrders(): Observable<Order[]> {
-    return this.http.get<Order[]>(`${environment.apiOrderServiceUrl}${this.orderUrl}`);
+    return new Observable((observer) => {
+      let eventSource = new EventSource(`${environment.apiOrderServiceUrl}${this.orderUrl}`);
+      eventSource.onmessage = (event) => {
+        let json = JSON.parse(event?.data);
+        this.orders.push(json);
+        this._zone.run(() => {
+          observer.next(this.orders);
+        });
+      }
+      eventSource.onerror = (error) => {
+        if(eventSource.readyState === 0) {
+          eventSource.close();
+          this._zone.run(() => {
+            observer.complete();
+          });
+        } else {
+          this._zone.run(() => {
+            observer.error('EventSource error: ' + error);
+          });
+        }
+      }
+    });
   }
 
   createOrder(request: Order): Observable<Order> {
